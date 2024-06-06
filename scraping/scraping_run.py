643a1a2e19714a0e40e5062a01ea03933f9866c1
nbetts2020 <114270814+nbetts2020@users.noreply.github.com>
2024-06-05 19:04:06 -0700

    Update scraping_run.py

--- scraping/scraping_run.py
+++ scraping/scraping_run.py
@@ -12,18 +12,16 @@ def scraping_main():
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     basketball_scraper = BasketballScraper()
-    d2_schools_df = basketball_scraper.d2_schools
-    #d2_schools_list = list(d2_schools_df['Name'])
-    d2_schools_list = ['Clemson University']
-    print(d2_schools_list)
-    data = basketball_scraper.data_all(d2_schools_list)
+    schools_df = basketball_scraper.d2_schools
+    schools_list = list(schools_df['Name'])
+    data = basketball_scraper.data_all(schools_list)
 
     cleaning_helper = CleaningHelpers(data)
     cleaning_helper.change_column_scale()
     data = cleaning_helper.get_columns()
     cleaning_helper.fill_missing_values()
-    # file_path = os.path.join('data', 'Training', 'basketball_data.csv')
-    # cleaning_helper.save_to_csv(file_path)
+    file_path = os.path.join('data', 'Training', 'basketball_data.csv')
+    cleaning_helper.save_to_csv(file_path)
 
 if __name__ == "__main__":
     scraping_main()
